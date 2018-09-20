0d3291f36ef13cb71389c9f4420acda32043a152
Steveennn <stevenvanderstel@hotmail.com>
2018-09-20 17:50:26 +0200

    Lesson 1 all works. Except fetching what actor is with what movie.

--- Oefeningen Les/Les1/Program.cs
+++ Oefeningen Les/Les1/Program.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using System.Linq;
+using Model;
+using System;
 
 namespace Les1
 {
@@ -6,7 +8,45 @@ namespace Les1
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            using (var db = new MovieContext()) {
+
+                /*//Adding a movie + actors to DB
+                Movie m = new Movie {
+                    Title = "No country for old men",
+                    Actors = new System.Collections.Generic.List<MovieActor> {
+                    new MovieActor{Actor = new Actor {Name = "Tommy Lee"}},
+                    new MovieActor{Actor = new Actor {Name = "Xavier Berdem"}}
+                    }
+                };
+                db.Movies.Add(m);
+                db.SaveChanges();*/
+
+                //Find and see the actors for each movie in DB
+                foreach (var movie in db.Movies) {
+                    Console.WriteLine("Found movie with title: " + movie.Title);
+                    var MovieID = movie.Id;
+                    //foreach (var movieActor in db.MovieActors.Where(ma => movie.Id == ma.MovieId)) {
+                    //    foreach (var actor in db.Actors.Where(movie.Id == MovieActor.MovieId)) {
+                    //        Console.WriteLine("Found actor with name: " + actor.Name);
+                    //    }
+                    //}
+                    var query = db.Actors.Where(a => a.Movies.Any(m => m.MovieId == MovieID)).ToList();
+                    var query1 = from actor in db.Actors where actor.Movies.Any(m => m.MovieId == MovieID) select actor;
+                    Console.WriteLine(query1);
+
+
+                    //foreach (var actor in db.Actors.Where(a => movie.Id == a.MovieId)) {
+                    //    Console.WriteLine("Found actor with name: " + actor.Name);
+                    //}
+                }
+
+                /*//Editing movie title name of the 1st movie in the DB
+                Movie foundMovie = db.Movies.Find(1);
+                Console.WriteLine("Found movie with title: " + foundMovie.Title);
+                foundMovie.Title = "White cats, Black cats...";
+                db.SaveChanges();
+                Console.WriteLine("Title changed to: " + foundMovie.Title);*/   
+            }
         }
     }
 }
--- Oefeningen Les/Les1/model.cs
+++ Oefeningen Les/Les1/model.cs
@@ -1,35 +1,45 @@
 using Microsoft.EntityFrameworkCore;
 using System.Collections.Generic;
 
-namespace Model
-{
-  public class MovieContext : DbContext
-  {
-    //this is actual entity object linked to the movies in our DB
-    public DbSet<Movie> Movies { get; set; }
-    //this is actual entity object linked to the actors in our DB
-    public DbSet<Actor> Actors { get; set; }
+namespace Model {
+    public class MovieContext : DbContext {
+        //this is actual entity object linked to the movies in our DB
+        public DbSet<Movie> Movies { get; set; }
+        //this is actual entity object linked to the actors in our DB
+        public DbSet<Actor> Actors { get; set; }
 
-    //this method is run automatically by EF the first time we run the application
-    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder){
-      //here we define the name of our database
-      optionsBuilder.UseNpgsql("User ID=postgres;Password=;Host=localhost;Port=5432;Database=MovieDB;Pooling=true;");
+        //this method is run automatically by EF the first time we run the application
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder) {
+            //here we define the name of our database
+            optionsBuilder.UseNpgsql("User ID=postgres;Password=steven;Host=localhost;Port=5432;Database=MovieDB;Pooling=true;");
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder) {
+            modelBuilder.Entity<MovieActor>().HasKey(t => new { t.ActorId, t.MovieId });
+            modelBuilder.Entity<MovieActor>().HasOne(ma => ma.Movie).WithMany(m => m.Actors).HasForeignKey(ma => ma.MovieId);
+            modelBuilder.Entity<MovieActor>().HasOne(ma => ma.Actor).WithMany(m => m.Movies).HasForeignKey(ma => ma.ActorId);
+        }
     }
-  }
 
-  //this is the typed representation of a movie in our project
-  public class Movie
-  {
-    public int Id { get; set; }
-    public string Title { get; set; }
-    public List<Actor> Actors { get; set; }
-  }
+    //this is the typed representation of a movie in our project
+    public class Movie {
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public virtual List<MovieActor> Actors { get; set; }
+    }
 
-  //this is the typed representation of an actor in our project
-  public class Actor
-  {
-    public int Id { get; set; }
-    public string Name { get; set; }
-    public int MovieId { get; set; }
-  }
+    //this is the typed representation of an actor in our project
+    public class Actor {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public virtual List<MovieActor> Movies { get; set; }
+    }
+
+    //this is the typed representation of our join table mapping a movie with an actor
+    public class MovieActor {
+        public int MovieId { get; set; }
+        public Movie Movie { get; set; }
+        public int ActorId { get; set; }
+        public Actor Actor { get; set; }
+    }
 }
