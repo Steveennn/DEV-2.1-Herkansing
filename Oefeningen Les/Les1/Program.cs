07ad06e7de60b012b4b1fa8676f768c3ddb86f27
Steveennn <stevenvanderstel@hotmail.com>
2018-09-27 13:30:14 +0200

    Maakt nu 1 actor aan niet meerdere

--- Oefeningen Les/Les1/Program.cs
+++ Oefeningen Les/Les1/Program.cs
@@ -11,50 +11,74 @@ namespace Les1
         {
             using (var db = new MovieContext()) {
 
-                /*//Adding a movie + actors to DB
-                Movie m = new Movie {
-                    Title = "The Lion King",
-                    Actors = new System.Collections.Generic.List<MovieActor> {
-                    new MovieActor{Actor = new Actor {Name = "Timon"}},
-                    new MovieActor{Actor = new Actor {Name = "Pumba"}},
-                    new MovieActor{Actor = new Actor {Name = "Simba"}}
-                    }
-                };
+                //Adding a movie + actors to DB
+                Actor a1 = new Actor {Name = "Tommy Lee"};
+                Actor a2 = new Actor {Name = "Xavier Berdem"};
                 Movie m1 = new Movie {
-                    Title = "Marvel's Avengers",
-                    Actors = new System.Collections.Generic.List<MovieActor> {
-                    new MovieActor{Actor = new Actor {Name = "Robert Downey Jr"}},
-                    new MovieActor{Actor = new Actor {Name = "Chris Hemsworth"}},
-                    new MovieActor{Actor = new Actor {Name = "Chris Evans"}}
-                    }
-                };
-                Movie m2 = new Movie {
-                    Title = "Thor",
-                    Actors = new System.Collections.Generic.List<MovieActor> {
-                    new MovieActor{Actor = new Actor {Name = "Chris Hemsworth"}}
-                    }
-                };
-                Movie m3 = new Movie {
-                    Title = "Black Panther",
-                    Actors = new System.Collections.Generic.List<MovieActor> {
-                    new MovieActor{Actor = new Actor {Name = "Chadwick Boseman"}},
-                    new MovieActor{Actor = new Actor {Name = "Chris Evans"}}
-                    }
-                };
-                Movie m4 = new Movie {
                     Title = "No country for old men",
                     Actors = new System.Collections.Generic.List<MovieActor> {
-                        new MovieActor{Actor = new Actor {Name = "Tommy Lee"}},
-                        new MovieActor{Actor = new Actor {Name = "Xavier Berdem"}}
+                        new MovieActor{Actor = a1},
+                        new MovieActor{Actor = a2}
+                    }
+                };
+
+                Actor a3 = new Actor {Name = "Timon"};
+                Actor a4 = new Actor {Name = "Pumba"};
+                Actor a5 = new Actor {Name = "Simba"};
+                Movie m2 = new Movie {
+                    Title = "The Lion King",
+                    Actors = new System.Collections.Generic.List<MovieActor> {
+                        new MovieActor{Actor = a3},
+                        new MovieActor{Actor = a4},
+                        new MovieActor{Actor = a5}
                     }
                 };
                 
-                db.Movies.Add(m);
+                Actor IronMan = new Actor {Name = "Robert Downey Jr"};
+                Actor Thor = new Actor {Name = "Chris Hemsworth"};
+                Actor CptAmerica = new Actor {Name = "Chris Evans"};
+                Movie m3 = new Movie {
+                    Title = "Marvel's Avengers",
+                    Actors = new System.Collections.Generic.List<MovieActor> {
+                        new MovieActor{Actor = IronMan},
+                        new MovieActor{Actor = Thor},
+                        new MovieActor{Actor = CptAmerica}
+                    }
+                };
+
+                Movie m4 = new Movie {
+                    Title = "Thor",
+                    Actors = new System.Collections.Generic.List<MovieActor> {
+                        new MovieActor{Actor = Thor}
+                    }
+                };
+                
+                Actor BlackPanther = new Actor {Name = "Chadwick Boseman"};
+                Movie m5 = new Movie {
+                    Title = "Black Panther",
+                    Actors = new System.Collections.Generic.List<MovieActor> {
+                        new MovieActor{Actor = BlackPanther},
+                        new MovieActor{Actor = CptAmerica}
+                    }
+                };
+
+                Movie m6 = new Movie {
+                    Title = "Avengers Infinity War",
+                    Actors = new System.Collections.Generic.List<MovieActor> {
+                        new MovieActor{Actor = BlackPanther},
+                        new MovieActor{Actor = CptAmerica},
+                        new MovieActor{Actor = IronMan},
+                        new MovieActor{Actor = Thor}
+                    }
+                };
+                                
                 db.Movies.Add(m1);
                 db.Movies.Add(m2);
                 db.Movies.Add(m3);
                 db.Movies.Add(m4);
-                db.SaveChanges();*/
+                db.Movies.Add(m5);
+                db.Movies.Add(m6);
+                db.SaveChanges();
 
                 //FEEDBACK VAN LES
                 foreach (var movie in db.Movies) {
@@ -69,6 +93,11 @@ namespace Les1
                         }
                     }
                 }
+
+                Console.WriteLine(" ");
+                Console.WriteLine("-----------------------------------------------");
+                Console.WriteLine(" ");
+
                 foreach (var actor in db.Actors) {
                     Console.WriteLine(actor.Name);
                     foreach (var ma in db.MovieActor) {
